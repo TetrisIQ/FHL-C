8a1414662b0f84b4341ffcc85dd74e7f8bcf916d
Alex <TetrisIQ@web.de>
2017-06-20 13:31:39 +0200

    .

---
+++ caesar/caesar.h
@@ -0,0 +1,13 @@
+/*
+ * caesar.h
+ *
+ *  Created on: 20.06.2017
+ *      Author: alex
+ */
+
+#ifndef CAESAR_H_
+#define CAESAR_H_
+
+void caesar(char text[], char offset);
+
+#endif /* CAESAR_H_ */
